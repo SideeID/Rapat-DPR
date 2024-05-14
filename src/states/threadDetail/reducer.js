5bf678afe13602c2b69f541a8262d069ecd97ee2
SideeID <sandipratama164026@gmail.com>
2024-05-14 10:09:47 +0700

    chore: Clean up code by removing commented out toggle code for upvote and downvote on thread comments

--- src/states/threadDetail/reducer.js
+++ src/states/threadDetail/reducer.js
@@ -1,3 +1,123 @@
+// import { ActionType } from './action';
+
+// const detailThreadReducer = (detailThread = null, action = {}) => {
+//   switch (action.type) {
+//     case ActionType.SET_THREAD_DETAIL:
+//       return action.payload.threadDetail;
+//     case ActionType.CLEAR_THREAD_DETAIL:
+//       return null;
+//     case ActionType.ADD_COMMENT:
+//       return {
+//         ...detailThread,
+//         comments: [action.payload.comment, ...detailThread.comments],
+//       };
+//     case ActionType.TOGGLE_UP_VOTE_THREAD_DETAIL:
+//       return {
+//         ...detailThread,
+//         upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
+//           ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
+//           : [...detailThread.upVotesBy, action.payload.userId],
+//         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
+//           ? detailThread.downVotesBy.filter(
+//               (id) => id !== action.payload.userId
+//             )
+//           : detailThread.downVotesBy,
+//       };
+//     case ActionType.TOGGLE_DOWN_VOTE_THREAD_DETAIL:
+//       return {
+//         ...detailThread,
+//         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
+//           ? detailThread.downVotesBy.filter(
+//               (id) => id !== action.payload.userId
+//             )
+//           : [...detailThread.downVotesBy, action.payload.userId],
+//         upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
+//           ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
+//           : detailThread.upVotesBy,
+//       };
+//     case ActionType.CLEAR_VOTE_THREAD_DETAIL:
+//       return {
+//         ...detailThread,
+//         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
+//           ? detailThread.downVotesBy.filter(
+//               (id) => id !== action.payload.userId
+//             )
+//           : detailThread.downVotesBy,
+//         upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
+//           ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
+//           : detailThread.upVotesBy,
+//       };
+//     case ActionType.TOGGLE_UP_VOTE_THREAD_COMMENT:
+//       return {
+//         ...detailThread,
+//         comments: detailThread.comments.map((comment) => {
+//           if (comment.id === action.payload.commentId) {
+//             return {
+//               ...comment,
+//               upVotesBy: comment.upVotesBy.includes(action.payload.userId)
+//                 ? comment.upVotesBy.filter((id) => id !== action.payload.userId)
+//                 : [...comment.upVotesBy, action.payload.userId],
+//               downVotesBy: comment.downVotesBy.includes(action.payload.userId)
+//                 ? comment.downVotesBy.filter(
+//                     (id) => id !== action.payload.userId
+//                   )
+//                 : comment.downVotesBy,
+//             };
+//           }
+//           return comment;
+//         }),
+//       };
+//     case ActionType.TOGGLE_DOWN_VOTE_THREAD_COMMENT:
+//       return {
+//         ...detailThread,
+//         comments: detailThread.comments.map((comment) => {
+//           if (comment.id === action.payload.commentId) {
+//             return {
+//               ...comment,
+//               downVotesBy: comment.downVotesBy.includes(action.payload.userId)
+//                 ? comment.downVotesBy.filter(
+//                     (id) => id !== action.payload.userId
+//                   )
+//                 : [...comment.downVotesBy, action.payload.userId],
+//               upVotesBy: comment.upVotesBy.includes(action.payload.userId)
+//                 ? comment.upVotesBy.filter((id) => id !== action.payload.userId)
+//                 : comment.upVotesBy,
+//             };
+//           }
+//           return comment;
+//         }),
+//       };
+
+//     case ActionType.CLEAR_VOTE_THREAD_COMMENT:
+//       return {
+//         ...detailThread,
+//         comments: detailThread.comments.map((comment) => {
+//           if (comment.id === action.payload.commentId) {
+//             return {
+//               ...comment,
+//               downVotesBy: detailThread.downVotesBy.includes(
+//                 action.payload.userId
+//               )
+//                 ? detailThread.downVotesBy.filter(
+//                     (id) => id !== action.payload.userId
+//                   )
+//                 : detailThread.downVotesBy,
+//               upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
+//                 ? detailThread.upVotesBy.filter(
+//                     (id) => id !== action.payload.userId
+//                   )
+//                 : detailThread.upVotesBy,
+//             };
+//           }
+//           return comment;
+//         }),
+//       };
+//     default:
+//       return detailThread;
+//   }
+// };
+
+// export default detailThreadReducer;
 import { ActionType } from './action';
 
 const detailThreadReducer = (detailThread = null, action = {}) => {
@@ -19,8 +139,8 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
           : [...detailThread.upVotesBy, action.payload.userId],
         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
           ? detailThread.downVotesBy.filter(
-            (id) => id !== action.payload.userId,
-          )
+              (id) => id !== action.payload.userId
+            )
           : detailThread.downVotesBy,
       };
     case ActionType.TOGGLE_DOWN_VOTE_THREAD_DETAIL:
@@ -28,8 +148,8 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
         ...detailThread,
         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
           ? detailThread.downVotesBy.filter(
-            (id) => id !== action.payload.userId,
-          )
+              (id) => id !== action.payload.userId
+            )
           : [...detailThread.downVotesBy, action.payload.userId],
         upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
           ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
@@ -40,53 +160,13 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
         ...detailThread,
         downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
           ? detailThread.downVotesBy.filter(
-            (id) => id !== action.payload.userId,
-          )
+              (id) => id !== action.payload.userId
+            )
           : detailThread.downVotesBy,
         upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
           ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
           : detailThread.upVotesBy,
       };
-    // case ActionType.TOGGLE_UP_VOTE_THREAD_COMMENT:
-    //   return {
-    //     ...detailThread,
-    //     comments: detailThread.comments.map((comment) => {
-    //       if (comment.id === action.payload.commentId) {
-    //         return {
-    //           ...comment,
-    //           upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
-    //             ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
-    //             : [...detailThread.upVotesBy, action.payload.userId],
-    //           downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
-    //             ? detailThread.downVotesBy.filter(
-    //               (id) => id !== action.payload.userId,
-    //             )
-    //             : detailThread.downVotesBy,
-    //         };
-    //       }
-    //       return comment;
-    //     }),
-    //   };
-    // case ActionType.TOGGLE_DOWN_VOTE_THREAD_COMMENT:
-    //   return {
-    //     ...detailThread,
-    //     comments: detailThread.comments.map((comment) => {
-    //       if (comment.id === action.payload.commentId) {
-    //         return {
-    //           ...comment,
-    //           downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
-    //             ? detailThread.downVotesBy.filter(
-    //               (id) => id !== action.payload.userId,
-    //             )
-    //             : [...detailThread.downVotesBy, action.payload.userId],
-    //           upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
-    //             ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
-    //             : detailThread.upVotesBy,
-    //         };
-    //       }
-    //       return comment;
-    //     }),
-    //   };
     case ActionType.TOGGLE_UP_VOTE_THREAD_COMMENT:
       return {
         ...detailThread,
@@ -99,8 +179,8 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
                 : [...comment.upVotesBy, action.payload.userId],
               downVotesBy: comment.downVotesBy.includes(action.payload.userId)
                 ? comment.downVotesBy.filter(
-                  (id) => id !== action.payload.userId,
-                )
+                    (id) => id !== action.payload.userId
+                  )
                 : comment.downVotesBy,
             };
           }
@@ -116,8 +196,8 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
               ...comment,
               downVotesBy: comment.downVotesBy.includes(action.payload.userId)
                 ? comment.downVotesBy.filter(
-                  (id) => id !== action.payload.userId,
-                )
+                    (id) => id !== action.payload.userId
+                  )
                 : [...comment.downVotesBy, action.payload.userId],
               upVotesBy: comment.upVotesBy.includes(action.payload.userId)
                 ? comment.upVotesBy.filter((id) => id !== action.payload.userId)
@@ -127,7 +207,6 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
           return comment;
         }),
       };
-
     case ActionType.CLEAR_VOTE_THREAD_COMMENT:
       return {
         ...detailThread,
@@ -135,14 +214,14 @@ const detailThreadReducer = (detailThread = null, action = {}) => {
           if (comment.id === action.payload.commentId) {
             return {
               ...comment,
-              downVotesBy: detailThread.downVotesBy.includes(action.payload.userId)
-                ? detailThread.downVotesBy.filter(
-                  (id) => id !== action.payload.userId,
-                )
-                : detailThread.downVotesBy,
-              upVotesBy: detailThread.upVotesBy.includes(action.payload.userId)
-                ? detailThread.upVotesBy.filter((id) => id !== action.payload.userId)
-                : detailThread.upVotesBy,
+              downVotesBy: comment.downVotesBy.includes(action.payload.userId)
+                ? comment.downVotesBy.filter(
+                    (id) => id !== action.payload.userId
+                  )
+                : comment.downVotesBy,
+              upVotesBy: comment.upVotesBy.includes(action.payload.userId)
+                ? comment.upVotesBy.filter((id) => id !== action.payload.userId)
+                : comment.upVotesBy,
             };
           }
           return comment;
