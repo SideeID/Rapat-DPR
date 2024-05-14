35d137ff1e37ec2d4784f5e63b5aac2aed3162a9
SideeID <sandipratama164026@gmail.com>
2024-05-14 10:37:31 +0700

    chage node js version

---
+++ src/components/LoginInput.test.jsx
@@ -0,0 +1,21 @@
+/**
+ * skenario testing
+ *
+ * - LoginInput component
+ *   - should handle username typing correctly
+ *   - should handle password typing correctly
+ *   - should call login function when login button is clicked
+ */
+
+import React from 'react';
+import { describe, it } from 'vitest';
+
+describe('LoginInput component', () => {
+  it('should handle username typing correctly', async () => {
+    // Arrange
+
+    // Action
+    
+    // Assert
+  });
+});
